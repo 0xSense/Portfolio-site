18d495970c79957d22f70298098aa3bd00446cdb
Sense0xDev <ragoo2244@gmail.com>
2022-10-15 13:01:17 -0400

    Intro Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Intro from '../src/components/Intro';
 
 function App() {
   return (
-    <div className="App">
+    <div className="h-100 w-100">
       <Intro />
     </div>
   );
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Intro = () => {
   return(
-    <div className="text-white m-100 w-full">
-      Sense
+    <div className="text-white m-100 w-full h-full">
+      <div>
+        <div className="mt-90 text-lg text-center" >Sense</div>
+      </div>
     </div>
   )
 }
