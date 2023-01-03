1828f5961fd3aa995f54d17d67a195c14b285a8c
Sense <105939929+0xSense@users.noreply.github.com>
2023-01-02 20:47:37 -0500

    Banner-Image

---
+++ src/components/banner/banner.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BannerImg from '../../assets/Banner-AI.png'
+import Styles from
+
+const Banner = () => {
+    return(
+        <div>
+            <img src={BannerImg} ></img>
+        </div>
+    )
+}
+
+export default Banner;
